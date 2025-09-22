f26f6b878c648c41b1bb96a9c0b315cad449b0c0
douglas-data-analyst <dr970788@gmail.com>
2025-09-22 17:33:04 -0300

    Update

--- Dominio/DTOs/loginDTO.cs
+++ Dominio/DTOs/loginDTO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MinimalApi.Models
+namespace MinimalApi.Dominio.DTOs
 {
     public class LoginDTO
     {
